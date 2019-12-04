62308b1b8f6f1c61c28c7c0ca06a9da08aaac48c
TanwiSharma54 <tsharma@madisoncollege.edu>
2019-12-04 15:14:26 -0600

    Get all recipes completed

---
+++ src/main/java/com/tanwisharma/controller/AllRecipesController.java
@@ -0,0 +1,30 @@
+package com.tanwisharma.controller;
+
+import com.tanwisharma.entity.Recipe;
+import com.tanwisharma.service.RecipeService;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "AllRecipesController", urlPatterns = {"/AllRecipesController"})
+public class AllRecipesController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        // List<Recipe> recipes = RecipeService.fetchRecipe(0, 50);
+        String recipes = RecipeService.fetchRecipe();
+
+        request.setAttribute("recipes", recipes);
+        request.getRequestDispatcher("Recipes.jsp").forward(request, response);
+    }
+}
--- target/cookbookapp/Recipes.jsp
+++ target/cookbookapp/Recipes.jsp
@@ -1,4 +1,5 @@
-<%--
+<%@ page import="javax.json.stream.JsonParser" %>
+<%@ page import="javax.json.JsonObject" %><%--
   Created by IntelliJ IDEA.
   User: student
   Date: 11/2/19
@@ -6,11 +7,19 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
 <h2>All recipe page</h2>
+<form action="<%= request.getContextPath() %>/AllRecipesController" method="GET" align="center">
+
+    <input type="submit" value="Get All Recipes" />
+</form>
+
+<p><c:out value="${recipes}"/></p>
+
 </body>
 </html>
